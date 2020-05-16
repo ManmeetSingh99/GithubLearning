1ea9f16df26824a6ada25ecb18d671d26ee71dde
Your Name <you@example.com>
2020-05-16 14:24:37 +0530

    updated for loops

--- for_loops.py
+++ for_loops.py
@@ -1,1 +1,19 @@
 for i in range(2):
+
+    a = int(input('Enter no.of rows::'))
+    b = int(input('Enter no.of columns::'))
+
+    matrix = []
+
+    for i in range(a):
+        c = []
+        for j in range(b):
+            j = int(input(f'Enter values for Matrix:: {i}  {j} '))
+            c.append(j)
+
+        matrix.append(c)
+
+    for i in range(a):
+        for j in range(b):
+            print(matrix[i][j], end=" ")
+        print()
